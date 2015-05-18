1596bdd30e8d7181bed87b8edd8b99f4dc29c5bb
kmoe <k.moe@oxon.org>
2015-05-18 19:37:15 +0100

    Add JavaScript fizzbuzz example

---
+++ javascript/fizzbuzz.js
@@ -0,0 +1,8 @@
+for(var i=1; i<=100; i++) {
+  var fizz = i % 3 === 0 ? 'fizz' : '';
+  var buzz = i % 5 === 0 ? 'buzz' : '';
+
+  var output = fizz || buzz ? fizz + buzz : i;
+
+  console.log(output);
+}
