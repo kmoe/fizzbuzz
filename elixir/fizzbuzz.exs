715ad6a436be4fd93c4a8d8f67aba298b1b365c0
kmoe <k.moe@oxon.org>
2015-05-25 13:21:39 +0100

    Add Elixir fizzbuzz example

---
+++ elixir/fizzbuzz.exs
@@ -0,0 +1,9 @@
+fizzbuzz = fn (_, 0, _) -> "fizz"
+              (_, _, 0) -> "buzz"
+              (_, 0, 0) -> "fizzbuzz"
+              (n, _, _) -> n
+end
+
+fizzbuzz = fn(n) -> fizzbuzz.(n, rem(n, 3), rem(n, 5)) end
+
+Enum.map 1..100, fn(i) -> IO.puts(fizzbuzz.(i)) end
