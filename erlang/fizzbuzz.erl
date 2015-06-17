6039c94d9b1d5b6c4e824567249369a443cd7d44
kmoe <k.moe@oxon.org>
2015-06-17 21:49:16 +0100

    First attempt at Erlang fizzbuzz

---
+++ erlang/fizzbuzz.erl
@@ -0,0 +1,11 @@
+-module(fizzbuzz).
+-export([fizzbuzz_output/0]).
+
+fizzbuzz_pattern(_, 0, 0) -> "fizzbuzz";
+fizzbuzz_pattern(_, 0, _) -> "fizz";
+fizzbuzz_pattern(_, _, 0) -> "buzz";
+fizzbuzz_pattern(N, _, _) -> integer_to_list(N).
+
+fizzbuzz(N) -> fizzbuzz_pattern(N, N rem 3, N rem 5).
+
+fizzbuzz_output() -> lists:map(fun(N) -> io:format(fizzbuzz(N)) end, lists:seq(1,100)).
