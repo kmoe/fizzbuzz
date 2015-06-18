ae13e197713eaf157e5fa8c655d4507f8c044a4c
kmoe <k.moe@oxon.org>
2015-06-18 18:58:21 +0100

    Add Go fizzbuzz

---
+++ go/fizzbuzz.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+  for i := 1; i <= 100; i++ {
+
+    fizz := ""    
+    buzz := ""
+
+    if i % 3 == 0 {
+      fizz = "fizz"
+    }
+   
+    if i % 5 == 0 {
+      buzz = "buzz"
+    }
+
+    if fizz != "" || buzz != "" {
+      fmt.Println(fizz + buzz)
+    } else {
+      fmt.Println(i)
+    }
+  }  
+}
