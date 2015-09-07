ef0648e6f2c0e178bedab276d543241e06a47071
Sami <sami.hs1@gmail.com>
2015-09-07 23:01:08 +0100

    Changed fizzbuzz.scala to use fold instead of map to prevent using function to return type Unit

--- scala/fizzbuzz.scala
+++ scala/fizzbuzz.scala
@@ -1,11 +1,15 @@
 object fizzbuzz extends App {
 
-  (1 to 100).map(x =>
-    x match {
-      case e if e % 15 == 0 => println("fizzbuzz")
-      case e if e % 5 == 0 => println("buzz")
-      case e if e % 3 == 0 => println("fizz")
-      case e => println(x.toString)
+  def fizzbuzz(number: Int): String = {
+    number match{
+      case n if n % 15 == 0 => "fizzbuzz"
+      case n if n % 3 == 0 => "fizz"
+      case n if n % 5 == 0 => "buzz"
+      case n  => n.toString
     }
-  )
+  }
+
+  val numbers = (1 to 100).toList
+  val answer =  (numbers.head.toString /: numbers.tail) ( _ + "\n" + fizzbuzz(_))
+  println(answer)
 }
