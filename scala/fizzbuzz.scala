f5629ea5e428edaddd41cefb207049a9b37af28f
Sami <sami.hs1@gmail.com>
2015-09-09 13:00:11 +0100

    Edited fizzbuzz.scala to use a map and mkString. Refactored names.

--- scala/fizzbuzz.scala
+++ scala/fizzbuzz.scala
@@ -1,6 +1,6 @@
 object fizzbuzz extends App {
 
-  def fizzbuzz(number: Int): String = {
+  def fizzbuzzIndividual(number: Int): String = {
     number match{
       case n if n % 15 == 0 => "fizzbuzz"
       case n if n % 3 == 0 => "fizz"
@@ -9,7 +9,6 @@ object fizzbuzz extends App {
     }
   }
 
-  val numbers = (1 to 100).toList
-  val answer =  (numbers.head.toString /: numbers.tail) ( _ + "\n" + fizzbuzz(_))
-  println(answer)
+  val fizzbuzz = (1 to 100).toList.map(fizzbuzzIndividual)
+  println(fizzbuzz.mkString("\n"))
 }
